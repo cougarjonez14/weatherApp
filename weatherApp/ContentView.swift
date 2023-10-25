6fe4b9f5f90e5f06a2059ef7e39490072e1da60b
stationAstral <cougarjonez@gmail.com>
2023-10-25 13:31:32 -0700

    fullCode

--- weatherApp/ContentView.swift
+++ weatherApp/ContentView.swift
@@ -11,38 +11,178 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack{
-            LinearGradient(colors: [.blue, .gray],
+            LinearGradient(colors: [.blue, .deeperSalmon],
                            startPoint: .top,
-                           endPoint: .bottomTrailing)       .edgesIgnoringSafeArea(.all).saturation(/*@START_MENU_TOKEN@*/1.25/*@END_MENU_TOKEN@*/)
+                           endPoint: .bottomTrailing)       .edgesIgnoringSafeArea(.all).saturation(1.25)
             VStack{
                 Image(systemName: "cloud.sun.fill")
                     .renderingMode(.original)
-                    .font(.system(size: 100, weight: .medium, design: .default))
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200, height: 120)
+                //.font(.system(size: 100, weight: .medium, design: .default))
                 
                 Text("Reno, NV")
-                    .font(.system(size: 32, weight: .medium, design: .default))
+                    .font(.system(size: 38, weight: .bold, design: .default))
+                    .kerning(4.0)
+                    .foregroundColor(.white)
+                Text("45°")
+                    .font(.system(size: 50, weight: .medium, design: .default))
                     .kerning(4.0)
                     .foregroundColor(.white)
                     .padding(.bottom, 500)
+                
             }
-            HStack {
+            HStack{
                 VStack {
-                    Text("63°")
+                    Text("TUE")
+                        .font(.system(size: 15, weight: .medium, design: .default))
+                        .bold()
+                        .kerning(4.0)
+                        .foregroundColor(.white)
+                    Image(systemName: "sun.max.fill")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 50, height: 50)
+                    Text("42°")
+                        .font(.system(size: 15, weight: .medium, design: .default))
+                        .bold()
+                        .kerning(4.0)
+                        .foregroundColor(.white)
+                }
+                .padding(.horizontal, 8)
+                
+                VStack {
+                    Text("WED")
+                        .font(.system(size: 15, weight: .medium, design: .default))
+                        .bold()
+                        .kerning(4.0)
+                        .foregroundColor(.white)
+                    Image(systemName: "cloud.sun.fill")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 50, height: 50)
+                    Text("35°")
+                        .font(.system(size: 15, weight: .medium, design: .default))
+                        .bold()
+                        .kerning(4.0)
+                        .foregroundColor(.white)
+                }
+                .padding(.horizontal, 8)
+                
+                VStack {
+                    Text("THUR")
+                        .font(.system(size: 15, weight: .medium, design: .default))
+                        .bold()
+                        .kerning(4.0)
+                        .foregroundColor(.white)
+                    Image(systemName: "cloud.snow.fill")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 50, height: 50)
+                    Text("29°")
+                        .font(.system(size: 15, weight: .medium, design: .default))
+                        .bold()
+                        .kerning(4.0)
+                        .foregroundColor(.white)
+                }
+                .padding(.horizontal, 8)
+                
+                VStack {
+                    Text("FRI")
+                        .font(.system(size: 15, weight: .medium, design: .default))
+                        .bold()
+                        .kerning(4.0)
+                        .foregroundColor(.white)
+                    Image(systemName: "sun.snow.fill")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 50, height: 50)
+                    Text("31°")
+                        .font(.system(size: 15, weight: .medium, design: .default))
+                        .bold()
+                        .kerning(4.0)
+                        .foregroundColor(.white)
+                }
+                .padding(.horizontal, 8)
+                
+                VStack {
+                    Text("SAT")
+                        .font(.system(size: 15, weight: .medium, design: .default))
+                        .bold()
+                        .kerning(4.0)
+                        .foregroundColor(.white)
+                    Image(systemName: "cloud.sun.fill")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 50, height: 50)
+                    Text("36°")
+                        .font(.system(size: 15, weight: .medium, design: .default))
+                        .bold()
+                        .kerning(4.0)
+                        .foregroundColor(.white)
+                }
+                .padding(.horizontal, 8)
+                
+            }
+            
+            
+            VStack {
+                Spacer()
+                Text("Tomorrow's Forecast:")
+                    .font(.system(size: 24, weight: .medium, design: .default))
+                    .kerning(4.0)
+                    .foregroundColor(.white)
+                
+                HStack {
+                    Text("42°")
                         .font(.system(size: 44, weight: .medium, design: .default))
                         .kerning(4.0)
                         .foregroundColor(.white)
-                        .padding()
+                        .padding(.trailing, 40.0)
                     
-                    Text("Clear")
+                    
+                    Text("Sunny")
                         .font(.system(size: 24, weight: .medium, design: .default))
                         .kerning(4.0)
                         .foregroundColor(.white)
-                        .padding()
+                    Image(systemName: "sun.max.fill")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 50, height: 50)
+                    
                 }
-                Spacer()
+                .padding(.bottom, 55)
+                
+                Button {
+                    print("Button pressed")
+                } label: {
+                    Text("Change City")
+                        .font(.system(size: 24, weight: .bold, design: .default))
+                        .kerning(4.0)
+                        .frame(width: 200, height: 50)
+                        .foregroundColor(.white)
+                        .background(Color.blue)
+                        .cornerRadius(15)
+                        .shadow(radius: 10.0)
+                    //make button wider
+                        .frame(width: 200, height: 50)
+                    
+                }
                 
                 
             }
+            .padding(.vertical, 45)
+            
+            
+            
+            
         }
     }
 }
