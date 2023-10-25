c73be1d2d57f6f4fe82d495d991e072370ffab17
stationAstral <cougarjonez@gmail.com>
2023-10-25 00:28:13 -0700

    Initial Commit

---
+++ weatherApp/weatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherAppApp.swift
+//  weatherApp
+//
+//  Created by stationAstral on 10/25/23.
+//
+
+import SwiftUI
+
+@main
+struct weatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
